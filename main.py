a64badea313819f76585acb1b1495a28b71dca1e
newkincode <maainnewkin59@gmail.com>
2023-06-18 19:26:34 +0900

    pml이라는 좋은걸 발견했지만 오류

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import tkinter
 import minecraft_launcher_lib
+from pmlauncher import pml, mlogin, mlaunchoption
+from pycraft import authentication
 import subprocess
 import sys
+import os
 import pprint
+
+p = os.getcwd() + "/game"
+pml.initialize(p)
+
+
+session = mlogin.session()
+session.username = "tester123"
+session.uuid = "uuid"
+session.access_token = "access_token"
 
 windows = tkinter.Tk()
 
