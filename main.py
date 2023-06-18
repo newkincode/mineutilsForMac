1a9d268338b34b08a01a4a5c6aba61919bebe59e
newkincode <maainnewkin59@gmail.com>
2023-06-18 18:55:41 +0900

    로그인구현을 못하겠어서 c#프로그램 빌드하고 그걸 파이썬으로 실행해서 리턴값을 가져와서 로그인 할것이다 그러므로 c# 프로그램을 만들었다

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import minecraft_launcher_lib
 import subprocess
 import sys
 import pprint
+
+windows = tkinter.Tk()
+
+
+
+windows.mainloop()
+
 pprint.pprint(minecraft_launcher_lib.utils.get_version_list()[0])
